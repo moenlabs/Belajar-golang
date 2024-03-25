50fda8b07872f601d81d7b8f623b775b22d81d89
moenlabs <munirvana1@gmail.com>
2024-03-25 21:50:57 +0700

    progress 32

---
+++ Golang-Library/7-math.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	fmt.Println(math.Ceil(2.30))
+	fmt.Println(math.Floor(7.70))
+	fmt.Println(math.Round(5.40))
+	fmt.Println(math.Max(30, 40))
+	fmt.Println(math.Min(21, 33))
+}
